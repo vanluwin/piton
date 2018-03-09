c1a06f10a4e269830c6e0bb25eccfdbd13547472
richard <richardsonsantiago@ufrn.edu.br>
2018-03-09 10:38:59 -0300

    gui bug solved

--- convoluiton.py
+++ convoluiton.py
@@ -1,24 +1,24 @@
 import cv2
 import numpy as np
 from scipy import signal
-
-'''
-#convolution example - Book CNN, Figure 3
+""" 
+# convolution example - Book CNN, Figure 3
 k = np.array([[1,1], [1,1]])
 img = np.array([[1, 2, 3, 1], [4, 5, 6, 1], [7, 8, 9, 1]])
 
 result =  signal.convolve2d(k, img, 'valid')
 
 print(result)
-'''
+"""
 
-#convolution example - Book CNN, Figure 4
+# convolution example - Book CNN, Figure 4
 lenna = cv2.imread('lenna.png')
 k = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
 
 cLenna = cv2.filter2D(lenna, -1, k)
+edgesLenna = cv2.Canny(lenna, 100, 200)
 
 cv2.imshow('Lenna', lenna)
 cv2.imshow('Convolved Lenna', cLenna)
+cv2.imshow('Edges Lenna', edgesLenna)
 cv2.waitKey(0)
-
