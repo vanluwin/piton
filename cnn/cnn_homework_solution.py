fc30c305efe84de9547a80db2f4cdd13bad00315
richard <richardsonsantiago@ufrn.edu.br>
2018-03-25 11:29:09 -0300

    single test

--- cnn/cnn_homework_solution.py
+++ cnn/cnn_homework_solution.py
@@ -72,7 +72,10 @@ classifier.fit_generator(training_set,
 
 import numpy as np
 from keras.preprocessing import image
-test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.load_img(
+    'dataset/single_prediction/cat_or_dog_1.jpg', 
+    target_size = (64, 64)
+)
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 result = classifier.predict(test_image)
