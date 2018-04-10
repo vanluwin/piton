d0c153e6a40916b6fa82329989503331f622dcbe
richard <richardsonsantiago@ufrn.edu.br>
2018-04-10 20:18:48 -0300

    working

--- cnn/cnn_homework_solution.py
+++ cnn/cnn_homework_solution.py
@@ -10,7 +10,7 @@
 # pip install --upgrade keras
 
 # Part 1 - Building the CNN
-
+"""
 # Importing the Keras libraries and packages
 from keras.models import Sequential
 from keras.layers import Conv2D
@@ -67,20 +67,26 @@ classifier.fit_generator(training_set,
                          epochs = 25,
                          validation_data = test_set,
                          validation_steps = 2000)
-
+"""
 # Part 3 - Making new predictions
 
 import numpy as np
 from keras.preprocessing import image
+from keras.models import load_model
+
+classifier = load_model('catDog.h5')
+
 test_image = image.load_img(
-    'dataset/single_prediction/cat_or_dog_1.jpg', 
+    'dataset/single_prediction/cat_or_dog_2.jpg', 
     target_size = (64, 64)
 )
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 result = classifier.predict(test_image)
-training_set.class_indices
+#training_set.class_indices
 if result[0][0] == 1:
     prediction = 'dog'
 else:
     prediction = 'cat'
+
+print(prediction)
